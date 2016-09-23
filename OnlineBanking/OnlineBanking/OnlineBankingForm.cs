129eaf892465c31060dfcc9200b817c9619097eb
lina-hs <lina.hannersanchez@gmail.com>
2016-09-23 12:23:32 +0200

    Updated constructor
    Initialize and set default values and properties

--- OnlineBanking/OnlineBanking/OnlineBankingForm.cs
+++ OnlineBanking/OnlineBanking/OnlineBankingForm.cs
@@ -15,9 +15,46 @@ namespace OnlineBanking
     public partial class OnlineBankingForm : Form
     {
 
+        List<Client> clients;
+
         public OnlineBankingForm()
         {
             InitializeComponent();
+
+            // Initalize clients
+            clients = new List<Client>();
+
+            // Add default clients
+            clients.Add(new Client() { firstName = "Kalle", lastName = "Anka" });
+            clients.Add(new Client() { firstName = "Musse", lastName = "Pigg" });
+            clients.Add(new Client() { firstName = "Janne", lastName = "Långben" });
+
+            // Set client listbox display member to the clients full name
+            lboxClients.DisplayMember = "fullName";
+
+            // Add accounts to each client and add the clients to the client listbox
+            for (int clientIndex = 0; clientIndex < clients.Count; clientIndex++)
+            {
+                Client client = clients[clientIndex];
+
+                // Create accounts
+                for (int i = 0; i < (clientIndex + 1); i++)
+                {
+                    client.accounts.Add(new Account());
+                }
+
+                // Add client to listbox
+                lboxClients.Items.Add(client);
+            }
+
+            // Add user type combo box alternatives
+            cboxUserType.Items.Add("Staff");    // usertypeID = 0
+            cboxUserType.Items.Add("Client");   // usertypeID = 1
+
+            // Hide panels
+
+            // Disable buttons
+
         }
 
         private void hidePanels()
