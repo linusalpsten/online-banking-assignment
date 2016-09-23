7ebbfcd5830e84f175c63491e2e6512912d76ea2
lina-hs <lina.hannersanchez@gmail.com>
2016-09-23 10:42:15 +0200

    Added library reference to onlineBankingLibrary

--- OnlineBanking/OnlineBanking/OnlineBankingForm.cs
+++ OnlineBanking/OnlineBanking/OnlineBankingForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using onlineBankingLibrary;
 
 namespace OnlineBanking
 {
+
     public partial class OnlineBankingForm : Form
     {
+
         public OnlineBankingForm()
         {
             InitializeComponent();
